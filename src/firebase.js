0845aa3f921abdfe2c084943b35e711c583787ee
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-08-23 12:12:40 +0600

    update

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,8 @@ const app = initializeApp({
   apiKey: "AIzaSyBJOihDOjyYwjqCntYJ7U-vtBkPCOnC49A",
   authDomain: "react-quiz-dev-8816b.firebaseapp.com",
   projectId: "react-quiz-dev-8816b",
+  databaseURL:
+    "https://react-quiz-dev-8816b-default-rtdb.asia-southeast1.firebasedatabase.app",
   storageBucket: "react-quiz-dev-8816b.appspot.com",
   messagingSenderId: "1828508950",
   appId: "1:1828508950:web:52a93c5989230fe5b72c37",
