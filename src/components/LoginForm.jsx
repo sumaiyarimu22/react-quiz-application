36debde7d4814522bbb1b492505c876f11fc26b9
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-08-23 11:36:55 +0600

    authentication done

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,63 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import TextInput from "./TextInput";
+import Form from "./Form";
+import Button from "./Button";
+import { useState } from "react";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  async function handlesubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      await login(email, password);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      setError("Failed to login");
+    }
+  }
+
+  return (
+    <Form style={{ height: "330px" }} onSubmit={handlesubmit}>
+      <TextInput
+        type="text"
+        placeholder="Enter email"
+        icon="alternate_email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextInput
+        type="password"
+        placeholder="Enter password"
+        icon="lock"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <Button type="submit" disabled={loading}>
+        <span>Submit Now</span>
+      </Button>
+      {error && <p className="error">{error}</p>}
+      <div className="info">
+        Dont have an account?<Link to="/signup"> Signup </Link>
+        instead.
+      </div>
+    </Form>
+  );
+};
+
+export default LoginForm;
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,9 @@
 import Button from "../Button";
 import Form from "../Form";
-import TextInput from "../TextInput";
+
 import Illustration from "../Illustration";
 
-import classes from "../../styles/Login.module.css";
+import LoginForm from "../LoginForm";
 
 const Login = () => {
   return (
@@ -11,27 +11,7 @@ const Login = () => {
       <h1>Login to your account</h1>
       <div className="column">
         <Illustration />
-        <Form className={classes.login}>
-          <TextInput
-            type="text"
-            placeholder="Enter email"
-            icon="alternate_email"
-            required
-          />
-          <TextInput
-            type="password"
-            placeholder="Enter password"
-            icon="lock"
-            required
-          />
-          <Button type="submit">
-            <span>Submit Now</span>
-          </Button>
-          <div className="info">
-            Dont have an account?<a href=""> Signup </a>
-            instead.
-          </div>
-        </Form>
+        <LoginForm />
       </div>
     </>
   );
